607174ef6389f49335c6fc4b562631a96eb47f7d
Callum Macrae <callumacrae95@gmail.com>
2011-03-06 16:34:37 +0000

    Added lots of comments

--- akismet.php
+++ akismet.php
@@ -12,6 +12,15 @@ class Akismet
 	private $config = array();
 	private $version = '0.0.1';
 
+	/**
+	 * Gets the configuration, attempts to find the URL if it isn't defined in
+	 * the configuration, and test whether API is valid and a connection can
+	 * be made. If it cannot, call the callback.
+	 *
+	 * @param string $fallback The name of the fallback to be called
+	 * @param array $fallback_args The params to be passed to the fallback
+	 * 	If not an array, it will be converted to one
+	 */
 	public function __construct($fallback = false, $fallback_args = false)
 	{
 		require('./config.php');
@@ -26,14 +35,25 @@ class Akismet
 		//check whether api key is valid
 		if (!$this->test() && $fallback)
 		{
+			//convert arguments to an array if they're not already
 			if (!is_array($fallback_args))
 			{
 				$fallback_args = array($fallback_args);
 			}
+			
+			//call the fallback
 			call_user_func_array($fallback, $fallback_args);
 		}
 	}
 
+	/**
+	 * Sends HTTP request to Akismet. Uses HTTP/1.0 because Akismet
+	 * told me to.
+	 *
+	 * @param string $request String to be passed via POST to Akismet
+	 * @param string $url URL for the request to be sent to
+	 * @param string $path Path for the request to be sent to
+	 */
 	private function http_post($request, $url, $path)
 	{
 		$useragent = $this->config['app'] . '/' . $this->config['app_ver'] . ' callumacrae/Akismet/ . ' . $this->version;
@@ -53,12 +73,23 @@ class Akismet
 		return $result;
 	}
 
+	/**
+	 * Test whether connection can be made
+	 */
 	public function test()
 	{
 		$test = $this->http_post('key=' . $this->config['api'] . '&blog=' . $this->config['url'], $this->config['akismet_server'], '/' . $this->config['akismet_version'] . '/verify-key');
 		return $test == 'valid';
 	}
 	
+	/**
+	 * Checks whether comment is spam. See the following page for info
+	 * of what to put in $user (although all the required info is done for you)
+	 * http://akismet.com/development/api/#comment-check
+	 *
+	 * @param array $user Array containing details of what should be sent
+	 * 	to Akismet
+	 */
 	public function check_spam($user)
 	{
 		$info = array(
@@ -97,6 +128,6 @@ class Akismet
 
 $akismet = new Akismet('trigger_error', 'Could not connect to Akismet');
 $array = array(
-	'comment_author'	=> 'test-viagra-123',
+	'comment_author'	=> 'test-viagra-123', //will always return true :D
 );
 var_dump($akismet->check_spam($array));
